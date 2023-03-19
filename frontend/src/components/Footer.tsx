9fa5d770aef0738b979af828bf7cbcceaa242256
Aadhib <aadhibnasserv.hussain@gmail.com>
2023-03-19 12:54:54 +0530

    Code Files
    File Upload

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ paddingBottom: 20, paddingTop: 20, textAlign: "center" }}
+      className="text-normal"
+    >
+      <div>Decentralized Voting System - Aadhib Nasser</div>
+    </footer>
+  );
+};
+
+export default Footer;
